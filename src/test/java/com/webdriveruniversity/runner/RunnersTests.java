f82c25c792a7e2b16fc73666ecd1612d9701717a
jatin <jatin@jatin-ThinkPad-X230>
2021-03-24 23:24:58 +0530

    Fifth trial

--- src/test/java/com/webdriveruniversity/runner/RunnersTests.java
+++ src/test/java/com/webdriveruniversity/runner/RunnersTests.java
@@ -11,7 +11,7 @@ import io.cucumber.junit.CucumberOptions;
         //tags = "@LinkTest",
         glue={"com.webdriveruniversity.stepDefinition"},
         plugin = {"pretty", "html:target/cucumber-reports"},
-        publish = true
+        publish = false
 ) 
 public class RunnersTests {
     public static void main(String[] args) {
